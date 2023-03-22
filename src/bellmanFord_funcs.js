5596f862b0a349c0480c13d0dbd6a1d8db784a0a
Hamza <mhamza@ryerson.ca>
2023-03-22 18:19:02 -0400

    bellmanFord initial commit

---
+++ src/bellmanFord_funcs.js
@@ -0,0 +1,30 @@
+// import example graph 1 json as a variable
+import graph1 from "./sampleGraph1.json" assert { type: "json" };
+// import example graph 2 json as a variable
+import graph2 from "./sampleGraph2.json" assert { type: "json" };
+
+// function to convert the graph to a edges list with each edge having format [start_node, end_node, weight]
+function convertObjectToEdgesList(graph) {
+	var edgesList = [];
+	var edges = graph.edges;
+	for (var i = 0; i < edges.length; i++) {
+		var curr_edge = edges[i];
+		var start_node = curr_edge.start.number;
+		var end_node = curr_edge.end.number;
+		var weight = curr_edge.weight;
+
+		edgesList.push([start_node, end_node, weight]);
+	}
+	return edgesList;
+}
+
+function printDistanceArray(distance) {
+  for (var i = 0; i < distance.length; i++) {
+    console.log("distance[" + i + "] = " + distance[i]);
+  }
+}
+
+// console.log(convertObjectToEdgesList(graph1));
+// console.log(convertObjectToEdgesList(graph2));
+
+export { convertObjectToEdgesList ,printDistanceArray};
