7e3ad5b5fa46a216c4990a0950fcbe8007616d26
Mueeza <ali.mueeza@gmail.com>
2023-03-28 07:46:40 -0400

    updated djikstra/canvas
    updated djikstra/canvas

---
+++ src/algorithms/djikstra.ts
@@ -0,0 +1,12 @@
+import {Graph, Node, Edge} from "../types/types.js";
+
+
+function djikstraAlgorithm(curr: any, start: number, end: number): any {
+
+    console.log("START", start);
+    console.log("END", end);
+    return curr
+}
+
+
+export { djikstraAlgorithm };
---
+++ src/types/types.ts
@@ -0,0 +1,26 @@
+interface Node {
+    x: number;
+    y: number;
+    radius: number;
+    color: string;
+    number: number;
+}
+  
+interface Edge {
+    start: Node;
+    end: Node;
+    weight: number;
+    color: string;
+}
+
+interface Graph{
+    nodes: Array<Node>;
+    edges: Array<Edge>;
+}
+  
+interface CanvasProps {
+    width: number;
+    height: number;
+}
+
+export type {Graph, Node, Edge};
