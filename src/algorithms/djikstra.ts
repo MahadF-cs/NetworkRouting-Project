bc629ecd139cc634caf8046310943cec36a308e7
Mahad Farah <mahadadanfarah@gmail.com>
2023-03-28 09:59:45 -0400

    Fixed forloop in djikstra.ts

--- src/algorithms/djikstra.ts
+++ src/algorithms/djikstra.ts
@@ -1,79 +1,70 @@
 // @ts-nocheck
-import {Graph, Node, Edge} from "../types/types.js";
+import { Graph, Node, Edge } from "../types/types.js";
 
 function convertObjectToEdgesList(graph) {
-	var edgesList = [];
-	var edges = graph.edges;
-	for (var i = 0; i < edges.length; i++) {
-		var curr_edge = edges[i];
-		var start_node = curr_edge.start.number;
-		var end_node = curr_edge.end.number;
-		var weight = curr_edge.weight;
+  let edgesList = [];
+  let edges = graph.Edges;
+  for (let i = 0; i < edges.length; i++) {
+    let curr_edge = edges[i];
+    let start_node = curr_edge.start.number;
+    let end_node = curr_edge.end.number;
+    let weight = curr_edge.weight;
 
-		edgesList.push([start_node, end_node, weight]);
-	}
-	return edgesList;
+    edgesList.push([start_node, end_node, weight]);
+  }
+  return edgesList;
 }
 
 function printDistanceArray(distance) {
-    for (var i = 0; i < distance.length; i++) {
-      console.log("distance[" + i + "] = " + distance[i]);
-    }
+  for (let i = 0; i < distance.length; i++) {
+    console.log("distance[" + i + "] = " + distance[i]);
+  }
 }
 
-function djikstraAlgorithm(curr, start, end){
+function djikstraAlgorithm(curr, start, end) {
+  // console.log("START", start);
+  // console.log("END", end);
+  //   console.log("curr", curr);
 
-    // console.log("START", start);
-    // console.log("END", end);
+  let distances = {};
+  let previous = {};
 
+  const edges = convertObjectToEdgesList(curr);
+  const num_vertices = curr.num_vertices;
+  const num_edges = curr.num_edges;
 
+  // initialize the queue
+  let queue = [];
 
-    let distances = {}; 
-    let previous = {};
+  for (let i = 0; i < num_vertices; i++) {
+    distances[i] = Infinity;
+    previous[i] = null;
+  }
 
-    const edges = convertObjectToEdgesList(curr);
-    const num_vertices = curr.num_vertices;
-	const num_edges = curr.num_edges;
+  distances[start] = 0;
+  queue.push(start);
 
-	// initialize the queue
-	var queue = [];
-
-	for (var i = 0; i < num_vertices; i++) {
-		distances[i] = Infinity;
-        previous[i] = null;
-	}
-
-	distances[start] = 0;
-    queue.push(start);
-
-    while(queue.length > 0){
-        var curr_node = queue.shift();
-        for (var i = 0; i < num_edges; i++) {
-            if (edges[i][0] == curr_node){
-                var neighbor = edges[i][1];
-                var weight = edges[i][2];
-                var alt = distances[curr_node] + weight;
-                if (alt < distances[neighbor]){
-                    distances[neighbor] = alt;
-                    previous[neighbor] = curr_node;
-                    queue.push(neighbor);
-                }
-
-            }   
+  while (queue.length > 0) {
+    let curr_node = queue.shift();
+    for (let i = 0; i < num_edges; i++) {
+      if (edges[i][0] == curr_node) {
+        let neighbor = edges[i][1];
+        let weight = edges[i][2];
+        let alt = distances[curr_node] + weight;
+        if (alt < distances[neighbor]) {
+          distances[neighbor] = alt;
+          previous[neighbor] = curr_node;
+          queue.push(neighbor);
         }
+      }
     }
+  }
 
+  for (let i = 0; i < distances.length; i++) {
+    console.log("distances[" + i + "] = " + distances[i]);
+  }
 
-
-
-
-    for (var i = 0; i < distance.length; i++) {
-        console.log("distance[" + i + "] = " + distance[i]);
-    }
-
-
-    // return curr
+  return curr;
 }
-
 
 export { djikstraAlgorithm };
