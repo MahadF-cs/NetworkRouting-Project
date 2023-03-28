60e0b38b1fdb789b559b7a183a0c7f14eae40f3b
Mahad Farah <mahadadanfarah@gmail.com>
2023-03-28 08:15:51 -0400

    Added Select Library to the Canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
-import {Graph, Node, Edge} from "../types/types.js";
+import { Graph, Node, Edge } from "../types/types.js";
 import { djikstraAlgorithm } from "../algorithms/djikstra.js";
+import Select from "react-select";
 
 interface CanvasProps {
   width: number;
@@ -20,8 +21,8 @@ const Canvas: React.FC<CanvasProps> = ({ width, height }) => {
   const [updateWeights, setUpdateWeights] = useState<boolean>(false);
   const [djikstra, setDjikstra] = useState<boolean>(false);
   const [bellmanFord, setBellmanFord] = useState<boolean>(false);
-  const [startNode, setStartNode] = useState<number>();
-  const [endNode, setEndNode] = useState<number>();
+  const [startNode, setStartNode] = useState<Number>();
+  const [endNode, setEndNode] = useState<Number>();
 
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
@@ -167,7 +168,7 @@ const Canvas: React.FC<CanvasProps> = ({ width, height }) => {
   };
 
   // console.log({ Nodes: nodes, Edges: edges });
-  console.log(djikstraAlgorithm({ Nodes: nodes, Edges: edges }, 0, 2))
+  console.log(djikstraAlgorithm({ Nodes: nodes, Edges: edges }, 0, 2));
 
   return (
     <div className="flex flex-col">
@@ -261,31 +262,42 @@ const Canvas: React.FC<CanvasProps> = ({ width, height }) => {
         </button>
 
         {/* dropdown menu thaat has all the nodes */}
-        <select
-          className="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-          onChange={(e) => {
-            setStartNode(parseInt(e.target.value));
+        <Select
+          options={nodes.map((node) => ({
+            value: node.number,
+            label: node.number,
+            node: node,
+          }))}
+          // change style color of options to black instead of white
+          styles={{
+            option: (provided, state) => ({
+              ...provided,
+              color: "black",
+            }),
           }}
-          defaultValue={-1}
-          // show "START NODE" on the dropdown menu
-        >
-          <option value={-1}>Not Selected</option>
-          {nodes.map((node) => (
-            <option value={node.number}>{node.number}</option>
-          ))}
-        </select>
-        <select
-          className="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Start Node"
           onChange={(e) => {
-            setEndNode(parseInt(e.target.value));
+            setStartNode(e?.value);
           }}
-          defaultValue={-1}
-        >
-          <option value={-1}>Not Selected</option>
-          {nodes.map((node) => (
-            <option value={node.number}>{node.number}</option>
-          ))}
-        </select>
+        />
+        <Select
+          options={nodes.map((node) => ({
+            value: node.number,
+            label: node.number,
+            node: node,
+          }))}
+          // change style color of options to black instead of white
+          styles={{
+            option: (provided, state) => ({
+              ...provided,
+              color: "black",
+            }),
+          }}
+          placeholder="End Node"
+          onChange={(e) => {
+            setEndNode(e?.value);
+          }}
+        />
       </div>
     </div>
   );
